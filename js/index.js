854982589f35d7f46fba2f624eb781d96a4f186a
Ruth Dejene <rsd2145@gmail.com>
2021-01-07 18:40:32 -0500

    js2

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
 
   $('h2').on('mouseover', function() {
-  $(this).animate({
-    "letter-spacing": "10px",
-    "font-size": "40px"
-  }, 1000);
+  $(this).animate({ "font-size" : "40px" }, 400)
+  .animate({ "letter-spacing" : "10px" }, 1000);
 });
